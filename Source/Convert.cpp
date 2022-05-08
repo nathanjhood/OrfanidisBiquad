2eee342113ad239252def5abcc790addbcfe0b25
Nathan Hood <nathanjhood@googlemail.com>
2022-05-08 12:00:53 +0200

    Implemented and enhanced Convert component
    Now running seperately from the Orfanidis Calculations component, and in variable precision

--- Source/Convert.cpp
+++ Source/Convert.cpp
@@ -3,9 +3,51 @@
 
     Convert.cpp
     Created: 8 May 2022 6:21:33am
-    Author:  natha
+    Author:  StoneyDSP
 
   ==============================================================================
 */
 
 #include "Convert.h"
+
+//==============================================================================
+template <typename SampleType>
+Conversion<SampleType>::Conversion()
+{
+
+}
+
+template <typename SampleType>
+void Conversion<SampleType>::prepare(juce::dsp::ProcessSpec& spec)
+{
+    jassert(spec.sampleRate > 0);
+
+    sampleRate = spec.sampleRate;
+}
+
+template <typename SampleType>
+void Conversion<SampleType>::reset()
+{
+
+}
+
+template <typename SampleType>
+SampleType Conversion<SampleType>::freqToHz(SampleType newFreq)
+{
+    jassert (SampleType(20.0) <= newFreq && newFreq <= SampleType(20000.0));
+
+    const SampleType minFreq = static_cast <SampleType>(sampleRate) / SampleType(24576.0);
+    const SampleType maxFreq = static_cast <SampleType>(sampleRate) / SampleType(2.125);
+
+    return jlimit(minFreq, maxFreq, newFreq);
+}
+
+template <typename SampleType>
+SampleType Conversion<SampleType>::hzToRadPerSamp(SampleType newHz)
+{
+    jassert(0 <= newHz && newHz <= ((sampleRate) / SampleType(2.0)));
+    return newHz * ((SampleType(2.0) * (juce::MathConstants<SampleType>::pi)) / static_cast <SampleType>(sampleRate));
+}
+
+template class Conversion<float>;
+template class Conversion<double>;
