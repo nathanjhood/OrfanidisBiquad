3983a308260b722139b3f34048d1232f272eb841
Nathan Hood <nathanjhood@googlemail.com>
2022-06-10 04:22:28 +0200

    Removed Timer callback in editor (for now)

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -10,12 +10,28 @@
 #include "PluginEditor.h"
 
 //==============================================================================
-OrfanidisBiquadAudioProcessorEditor::OrfanidisBiquadAudioProcessorEditor (OrfanidisBiquadAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor(p)
+OrfanidisBiquadAudioProcessorEditor::OrfanidisBiquadAudioProcessorEditor (OrfanidisBiquadAudioProcessor& p, APVTS& apvts, juce::UndoManager& um)
+    :
+    juce::AudioProcessorEditor(&p),
+    audioProcessor(p),
+    state(apvts),
+    undoManager(um),
+    subComponents(p, apvts),
+    undoButton("Undo"),
+    redoButton("Redo")
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize(400, 300);
+
+    setSize(425, 250);
+    addAndMakeVisible(subComponents);
+    addAndMakeVisible(undoButton);
+    addAndMakeVisible(redoButton);
+    undoButton.onClick = [this] { undoManager.undo(); };
+    redoButton.onClick = [this] { undoManager.redo(); };
+    setResizable(true, true);
+
+    //startTimerHz(24);
 }
 
 
@@ -23,19 +39,34 @@ OrfanidisBiquadAudioProcessorEditor::~OrfanidisBiquadAudioProcessorEditor()
 {
 }
 
-//==============================================================================
+////==============================================================================
+//void OrfanidisBiquadAudioProcessorEditor::timerCallback()
+//{
+//    undoManager.beginNewTransaction();
+//}
+
 void OrfanidisBiquadAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    g.fillAll(juce::Colours::midnightblue);
 
-    g.setColour(juce::Colours::white);
+    //// draw an outline around the component
+    /*g.setColour(juce::Colours::hotpink);
+    g.drawRect(getLocalBounds(), 2);*/
+
+    // Add project info text to background here
+    g.setColour(juce::Colours::antiquewhite);
     g.setFont(15.0f);
-    g.drawFittedText("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText(ProjectInfo::companyName, getLocalBounds(), juce::Justification::topLeft, 1);
+    g.drawFittedText(ProjectInfo::projectName, getLocalBounds(), juce::Justification::topRight, 1);
+    g.drawFittedText(ProjectInfo::versionString, getLocalBounds(), juce::Justification::bottomLeft, 1);
 }
 
 void OrfanidisBiquadAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+
+    subComponents.resized();
+    subComponents.setBounds(0, 25, getWidth(), getHeight());
 }
